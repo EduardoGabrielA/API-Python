5a78afbbeab2b8dbb786c8fd56717c45e0684346
Eduardo Gabriel <egaa2@hotmail.com>
2021-01-27 05:38:48 +0000

    sexto commit translate

--- todos/translate.py
+++ todos/translate.py
@@ -11,23 +11,28 @@ print(result['TranslatedText'])
 import json
 import boto3
 
+from todos import decimalencoder
+
 def lambda_handler(event, context): #event, context
     # TODO implement
-    client = boto3.client('translate', region_name="us-east-1")
+    """client = boto3.client('translate', region_name="us-east-1")
     text = "hola, mi nombre es Eduardo Gabriel"
     lenguajeAtraducir = "fr"
     
     result = client.translate_text(Text=text, SourceLanguageCode="auto", 
         TargetLanguageCode=lenguajeAtraducir)
     print(result['TranslatedText'])
-    
+    """
     capturaRoute = event['pathParameters']['language']
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps(capturaRoute)
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(capturaRoute,
+                           cls=decimalencoder.DecimalEncoder)
     }
 
+    return response
 """
     return {
         'statusCode': 200,
