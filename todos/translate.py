956682b2fc42ac0b4504a68a7efa50ac85a06ca5
Eduardo Gabriel <egaa2@hotmail.com>
2021-01-28 15:21:50 +0000

    corrigiendo la traducci+on

--- todos/translate.py
+++ todos/translate.py
@@ -9,7 +9,7 @@ dynamodb = boto3.resource('dynamodb')
 client = boto3.client('translate', region_name="us-east-1")
 
 def translate(event, context):
-    # TODO implement
+    # Creando variable table 
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
     # fetch todo from the database
@@ -19,16 +19,16 @@ def translate(event, context):
         }
     )
     lenguajeAtraducir = event['pathParameters']['language']
-    text = "Quiero funcionar correctamente"
-    #Traduciendo petición de DynamoDB
+    
+    # Traduciendo petición de DynamoDB
     result = client.translate_text(Text=toTraduce['Item']['text'], SourceLanguageCode="auto", 
         TargetLanguageCode=lenguajeAtraducir)
     
-    # create a response
+    # create a response.... EN caso de querer solo el texto usar: result['TranslatedText']
     response = {
         "statusCode": 200,
-        "body": json.dumps(result['TranslatedText']['SourceLanguageCode'])#, toTraduce['Item']['text']  result['TranslatedText']['SourceLanguageCode']['TargetLanguageCode']
-                           #cls=decimalencoder.DecimalEncoder)
+        "body": json.dumps(result) 
+                           cls=decimalencoder.DecimalEncoder)
     }
 
     return response
